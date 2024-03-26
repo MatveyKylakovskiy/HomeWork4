98048e2c772ab74eb4da94e56f13fc75e019febc
Matveykylak <matveykylak@gmail.com>
2024-03-26 22:59:36 +0300

    First task

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿//First task
+int[] array = { 1, 2, 3, 2, 5, 6, 7, 8, 9 };
+
+Console.WriteLine("Enter a number to check");
+
+int number = int.Parse(Console.ReadLine());
+int counter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (number == array[i])
+    {
+        counter++;
+    }
+}
+
+switch (counter)
+{
+    case 0:
+        {
+            Console.WriteLine($"The number {number} is not included in the array");
+            break;
+        }
+
+    default:
+        {
+            Console.WriteLine($"The number {number} is included in the array");
+            break;
+        }
+}

d00e4a4cf3c24c6fe8c126ed8963b0bbcf7ceea4
Matveykylak <matveykylak@gmail.com>
2024-03-26 23:38:56 +0300

    Second Task

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,5 +1,5 @@
 ﻿//First task
-int[] array = { 1, 2, 3, 2, 5, 6, 7, 8, 9 };
+/*int[] array = { 1, 2, 3, 2, 5, 6, 7, 8, 9 };
 
 Console.WriteLine("Enter a number to check");
 
@@ -27,4 +27,51 @@ switch (counter)
             Console.WriteLine($"The number {number} is included in the array {counter} times");
             break;
         }
+}*/
+
+//Second task
+int[] array = { 1, 3, 2, 3, 4, 5, 4, 3};
+
+Console.WriteLine("Enter a number to check");
+
+int number = int.Parse(Console.ReadLine());
+int counter1 = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (number == array[i])
+    {
+        counter1++;
+    }
+    
+}
+
+switch (counter1) { 
+    case 0:
+        {
+            Console.WriteLine($"The number {number} is not included in the array");
+            break;
+        }
+
+    default:
+        {
+            int[] arrayNew = new int[array.Length - counter1];
+            int counter2 = 0;
+
+            foreach (int item in array)
+            {
+
+                if (number != item)
+                {
+                    arrayNew[counter2] = item;
+                    counter2++;
+                }
+            }
+
+            Console.WriteLine(string.Join(",", array));
+            Console.WriteLine(string.Join(",", arrayNew));
+        }
+        break;
+
 }
